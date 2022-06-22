13815f4f0d039df418926cdb970496429abb2fd2
dczopek <dawid.czopek@proget.pl>
2022-06-22 15:01:42 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,12 +64,12 @@ android {
 dependencies {
     implementation(project(":main"))
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.20")
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("net.lingala.zip4j:zip4j:2.7.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("net.lingala.zip4j:zip4j:2.9.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
