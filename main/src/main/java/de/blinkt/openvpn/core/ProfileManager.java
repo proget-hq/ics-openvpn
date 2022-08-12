ed204c11c184abc1e431969a14d7da45f04b6d6c
Arne Schwabe <arne@rfc2549.org>
2022-08-12 14:41:22 +0200

    Mark all methods in profile manager synchronized to avoid race conditions

--- main/src/main/java/de/blinkt/openvpn/core/ProfileManager.java
+++ main/src/main/java/de/blinkt/openvpn/core/ProfileManager.java
@@ -278,7 +278,7 @@ public class ProfileManager {
      * profiles
      * @param context
      */
-    public void refreshVPNList(Context context)
+    public synchronized void refreshVPNList(Context context)
     {
         SharedPreferences listpref = Preferences.getSharedPreferencesMulti(PREFS_NAME, context);
         Set<String> vlist = listpref.getStringSet("vpnlist", null);
@@ -302,7 +302,7 @@ public class ProfileManager {
         }
     }
 
-    private void loadVPNList(Context context) {
+    private synchronized void loadVPNList(Context context) {
         profiles = new HashMap<>();
         SharedPreferences listpref = Preferences.getSharedPreferencesMulti(PREFS_NAME, context);
         Set<String> vlist = listpref.getStringSet("vpnlist", null);
@@ -317,7 +317,7 @@ public class ProfileManager {
         }
     }
 
-    private void loadVpnEntry(Context context, String vpnentry) {
+    private synchronized void loadVpnEntry(Context context, String vpnentry) {
         ObjectInputStream vpnfile = null;
         try {
             FileInputStream vpInput;
