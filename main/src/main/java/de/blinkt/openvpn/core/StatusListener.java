964a022833a968dfe1203a7d71f3b65c68526a92
Arne Schwabe <arne@rfc2549.org>
2023-11-24 15:52:52 +0100

    Allow OpenVPNService to request VPN permission if it is missing
    With other services no longer being able to easily request permissions
    the service needs now its own way to request permissions.

--- main/src/main/java/de/blinkt/openvpn/core/StatusListener.java
+++ main/src/main/java/de/blinkt/openvpn/core/StatusListener.java
@@ -26,7 +26,6 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * Created by arne on 09.11.16.
@@ -123,7 +122,7 @@ public class StatusListener implements VpnStatus.LogListener {
     private Context mContext;
     private String pkgName = "(packageName not yet set)";
 
-    public void init(Context c) {
+    void init(Context c) {
         pkgName = c.getPackageName();
         Intent intent = new Intent(c, OpenVPNStatusService.class);
         intent.setAction(OpenVPNService.START_SERVICE);
@@ -166,18 +165,23 @@ public class StatusListener implements VpnStatus.LogListener {
     @Override
     public void newLog(LogItem logItem) {
         String tag = pkgName + "(OpenVPN)";
-        long logAge = System.currentTimeMillis() - logItem.getLogtime();
-        if (logAge > 5000)
-        {
-            tag += String.format(Locale.US, "[%ds ago]", logAge/1000 );
-        }
-
         switch (logItem.getLogLevel()) {
-            case INFO -> Log.i(tag, logItem.getString(mContext));
-            case DEBUG -> Log.d(tag, logItem.getString(mContext));
-            case ERROR -> Log.e(tag, logItem.getString(mContext));
-            case VERBOSE -> Log.v(tag, logItem.getString(mContext));
-            default -> Log.w(tag, logItem.getString(mContext));
+            case INFO:
+                Log.i(tag, logItem.getString(mContext));
+                break;
+            case DEBUG:
+                Log.d(tag, logItem.getString(mContext));
+                break;
+            case ERROR:
+                Log.e(tag, logItem.getString(mContext));
+                break;
+            case VERBOSE:
+                Log.v(tag, logItem.getString(mContext));
+                break;
+            case WARNING:
+            default:
+                Log.w(tag, logItem.getString(mContext));
+                break;
         }
 
     }
